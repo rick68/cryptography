1b54ef51f4ae85a50a169b9ed216b8845e2d1d86
Rick Yang <rick68@gmail.com>
2013-11-25 19:18:49 +0800

    Add Vigenere Cipher

---
+++ ciphers/vigenere.hpp
@@ -0,0 +1,179 @@
+//
+// vigenere.hpp
+// ~~~~~~~~~~~~
+//
+// Copyright (c) 2013 Rick Yang (rick68 at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef ATHENA_CIPHERS_VIGENERE_HPP
+#define ATHENA_CIPHERS_VIGENERE_HPP
+
+// STL
+#include <cstddef>
+#include <string>
+#include <memory>
+
+// Boost.Config
+#include <boost/config/suffix.hpp>
+
+// Boost.Preprocessor
+#include <boost/preprocessor/punctuation/comma.hpp>
+
+// Boost.Utility
+#include <boost/assert.hpp>
+
+// Athena.Ciphers
+#include "detail/config.hpp"
+#include "detail/left_rotate.hpp"
+#include "detail/random_access.hpp"
+#include "detail/first.hpp"
+#include "detail/last.hpp"
+#include "detail/length.hpp"
+#include "table_array.hpp"
+
+namespace athena {
+namespace ciphers {
+
+template <typename Alphabet, std::size_t X, std::size_t Y>
+struct vigenere_table
+{
+  BOOST_STATIC_CONSTANT(std::size_t, value =
+      detail::random_access<
+          typename detail::rot<
+              Alphabet BOOST_PP_COMMA()
+              X
+          >::type BOOST_PP_COMMA()
+          Y
+      >::value
+  );
+};
+
+template <
+    typename Alphabet,
+    typename String = std::basic_string<
+        char_type,
+        std::char_traits<char_type>,
+        std::allocator<char_type>
+    >
+>
+struct vigenere
+{
+  BOOST_STATIC_CONSTANT(std::size_t, first = detail::first<Alphabet>::value);
+  BOOST_STATIC_CONSTANT(std::size_t, last = detail::last<Alphabet>::value);
+  BOOST_STATIC_CONSTANT(std::size_t, length =
+      detail::length<Alphabet>::value * detail::length<Alphabet>::value);
+
+  typedef Alphabet alphabet_type;
+  typedef String string_type;
+  template <std::size_t X, std::size_t Y> struct table
+    : vigenere_table<alphabet_type, X, Y> {};
+  typedef table_array<alphabet_type, vigenere_table> array_type;
+
+  vigenere(const string_type& keyword)
+    : keyword_(keyword) {}
+
+  const string_type& keyword() const { return keyword_; }
+  string_type& keyword() { return keyword_; }
+
+  inline static typename string_type::value_type
+  encode(typename string_type::value_type c,
+         typename string_type::value_type key)
+  {
+    BOOST_ASSERT(key >= first && key <= last);
+
+    if (c >= first && c <= last)
+      c = array_type::data[(c - first) + (key - first) * alphabet_number];
+
+    return c;
+  }
+
+  inline string_type& encode(string_type& text) const
+  {
+    const typename string_type::size_type& tsize = text.size();
+    const typename string_type::size_type& ksize = keyword_.size();
+
+    if (tsize == 0)
+      return text;
+
+    for (std::size_t i = 0, n = 0; i < tsize; ++i)
+    {
+      typename string_type::reference c = text[i];
+
+      if (c >= first && c <= last)
+      {
+        typename string_type::const_reference& k = keyword_[n];
+
+        if (++n >= ksize)
+          n = 0;
+
+        c = array_type::data[(c - first) + (k - first) * alphabet_number];
+      }
+    }
+
+    return text;
+  }
+
+  inline static typename string_type::value_type
+  decode(typename string_type::value_type c,
+         typename string_type::value_type key)
+  {
+    BOOST_ASSERT(key >= first && key <= last);
+
+    if (c >= first && c <= last)
+    {
+      typename string_type::const_pointer ptr =
+          &array_type::data[(key - first) * alphabet_number];
+      std::size_t i = 0;
+
+      for (; i < alphabet_number; ++i)
+        if (*ptr++ == c)
+          break;
+
+      c = first + i;
+    }
+
+    return c;
+  }
+
+  inline string_type& decode(string_type& text) const
+  {
+    const typename string_type::size_type& tsize = text.size();
+    const typename string_type::size_type& ksize = keyword_.size();
+
+    if (tsize == 0) return text;
+
+    for (std::size_t i = 0, n = 0; i < tsize; ++i)
+    {
+      typename string_type::reference c = text[i];
+
+      if (c >= first && c <= last)
+      {
+        typename string_type::const_reference& k = keyword_[n];
+        typename string_type::const_pointer ptr =
+            &array_type::data[(k - first) * alphabet_number];
+        std::size_t j = 0;
+
+        if (++n >= ksize)
+          n = 0;
+
+        for (j = 0; j < alphabet_number; ++j)
+          if (*ptr++ == c)
+            break;
+
+        c = first + j;
+      }
+    }
+    return text;
+  }
+
+private:
+  string_type keyword_;
+};
+
+} // namespace ciphers
+} // namespace athena
+
+#endif // ATHENA_CIPHERS_VIGENERE_HPP
---
+++ test/vigenere.cpp
@@ -0,0 +1,165 @@
+//
+// vigenere.cpp
+// ~~~~~~~~~~~~
+//
+// Copyright (c) 2013 Rick Yang (rick68 at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+// Disable autolinking for unit tests.
+#if !defined(BOOST_ALL_NO_LIB)
+#define BOOST_ALL_NO_LIB 1
+#endif // !defined(BOOST_ALL_NO_LIB)
+
+// Test that header file is self-contained.
+#include "../ciphers/vigenere.hpp"
+
+#include "unit_test.hpp"
+
+// STL
+#include <iostream>
+#include <cstddef>
+
+// Boost.Preproccess
+#include <boost/preprocessor/repetition/repeat.hpp>
+#include <boost/preprocessor/tuple/elem.hpp>
+#include <boost/preprocessor/punctuation/comma.hpp>
+#include <boost/preprocessor/cat.hpp>
+
+//------------------------------------------------------------------------------
+
+// vigenere_compile test
+// ~~~~~~~~~~~~~~~~~~~~~
+// The following test checks that all public member functions on the class
+// vigenere compile and link correctly. Runtime failures are ignored.
+
+namespace vigenere_compile {
+
+#define UNIT_TEST_VIGENERE_COMPILE_GET_SEQUENCE_PP_PEPEAT_MACRO(z, x, alp) \
+  BOOST_PP_REPEAT( \
+      ATHENA_CIPHERS_ALPHABET_NUMBER, \
+      UNIT_TEST_VIGENERE_COMPILE_GET_ELEM_PP_PEPEAT_MACRO, \
+      (alp, x)) \
+  /**/
+
+#define UNIT_TEST_VIGENERE_COMPILE_GET_ELEM_PP_PEPEAT_MACRO(z, y, tuple2) \
+  typedef vigenere_table< \
+      BOOST_PP_TUPLE_ELEM(2, 0, tuple2) BOOST_PP_COMMA() \
+      BOOST_PP_TUPLE_ELEM(2, 1, tuple2) BOOST_PP_COMMA() \
+      y> \
+  BOOST_PP_CAT(vt_, \
+  BOOST_PP_CAT(BOOST_PP_TUPLE_ELEM(2, 1, tuple2), \
+  BOOST_PP_CAT(_, \
+  BOOST_PP_CAT(y, \
+  BOOST_PP_CAT(_, \
+  BOOST_PP_CAT(BOOST_PP_TUPLE_ELEM(2, 0, tuple2), _t)))))); \
+  /**/
+
+void test()
+{
+  using namespace athena::ciphers;
+
+  typedef vigenere<uppercase> vu_t;
+  typedef vigenere<lowercase> vl_t;
+
+  BOOST_PP_REPEAT(
+      ATHENA_CIPHERS_ALPHABET_NUMBER,
+      UNIT_TEST_VIGENERE_COMPILE_GET_SEQUENCE_PP_PEPEAT_MACRO,
+      uppercase)
+
+  BOOST_PP_REPEAT(
+      ATHENA_CIPHERS_ALPHABET_NUMBER,
+      UNIT_TEST_VIGENERE_COMPILE_GET_SEQUENCE_PP_PEPEAT_MACRO,
+      lowercase)
+
+  vu_t vu("HELLO");
+  vl_t vl("world");
+}
+
+} // namespace vigenere_compile
+
+//------------------------------------------------------------------------------
+
+// vigenere_runtime test
+// ~~~~~~~~~~~~~~~~~~~~~
+// The following test checks that the various public member functions meet the
+// necessary postconditions.
+
+namespace vigenere_runtime {
+
+#define UNIT_TEST_VIGENERE_RUNTIME_SHOW_SEQUENCE_PP_PEPEAT_MACRO(z, x, alp) \
+  BOOST_PP_REPEAT( \
+      ATHENA_CIPHERS_ALPHABET_NUMBER, \
+      UNIT_TEST_VIGENERE_RUNTIME_SHOW_ELEM_PP_PEPEAT_MACRO, \
+      (alp, x)) \
+  std::cout << std::endl; \
+  /**/
+
+#define UNIT_TEST_VIGENERE_RUNTIME_SHOW_ELEM_PP_PEPEAT_MACRO(z, y, tuple2) \
+  std::cout << (char_type) vigenere_table< \
+      BOOST_PP_TUPLE_ELEM(2, 0, tuple2) BOOST_PP_COMMA() \
+      BOOST_PP_TUPLE_ELEM(2, 1, tuple2) BOOST_PP_COMMA() \
+      y>::value; \
+  /**/
+
+void vigenere_table_test()
+{
+  using namespace athena::ciphers;
+
+  BOOST_PP_REPEAT(
+      ATHENA_CIPHERS_ALPHABET_NUMBER,
+      UNIT_TEST_VIGENERE_RUNTIME_SHOW_SEQUENCE_PP_PEPEAT_MACRO,
+      uppercase)
+  std::cout << vigenere<uppercase>::array_type::data << std::endl;
+
+  BOOST_PP_REPEAT(
+      ATHENA_CIPHERS_ALPHABET_NUMBER,
+      UNIT_TEST_VIGENERE_RUNTIME_SHOW_SEQUENCE_PP_PEPEAT_MACRO,
+      lowercase)
+  std::cout << vigenere<lowercase>::array_type::data << std::endl;
+}
+
+void test()
+{
+  using namespace athena::ciphers;
+
+  vigenere_table_test();
+
+  typedef vigenere<uppercase> vu_t;
+  typedef vigenere<lowercase> vl_t;
+
+  vu_t vu("HELLO");
+  vl_t vl("world");
+
+  vu_t::string_type vu_kw = vu.keyword();
+  vl_t::string_type vl_kw = vl.keyword();
+
+  ATHENA_CIPHERS_CHECK(vu_kw == "HELLO");
+  ATHENA_CIPHERS_CHECK(vl_kw == "world");
+
+  std::string utext = "ATTACKATDAWN";
+  std::string ltext = "attackatdawn";
+
+  for (std::size_t i = 0, n = 0; i < utext.size(); ++i)
+  {
+    ATHENA_CIPHERS_CHECK
+        (vu.decode(vu.encode(utext[i], vu_kw[n]), vu_kw[n]) == utext[i]);
+    ATHENA_CIPHERS_CHECK
+        (vl.decode(vl.encode(ltext[i], vl_kw[n]), vl_kw[n]) == ltext[i]);
+  }
+
+  ATHENA_CIPHERS_CHECK(vu.decode(vu.encode(utext)) == "ATTACKATDAWN");
+  ATHENA_CIPHERS_CHECK(vl.decode(vl.encode(ltext)) == "attackatdawn");
+
+}
+
+} // namespace vigenere_runtime
+
+ATHENA_CIPHERS_TEST_SUITE
+(
+  "vigenere",
+  ATHENA_CIPHERS_TEST_CASE(vigenere_compile::test)
+  ATHENA_CIPHERS_TEST_CASE(vigenere_runtime::test)
+)
