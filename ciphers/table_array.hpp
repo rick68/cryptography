65657da1861084bf32d4fbd679eb6bfe7f8546f8
Rick Yang <rick68@gmail.com>
2013-11-25 17:22:34 +0800

    expanded table_array
    improve code readable

--- ciphers/table_array.hpp
+++ ciphers/table_array.hpp
@@ -18,7 +18,9 @@
 #include <boost/config/suffix.hpp>
 
 // Boost.Preprocessor
-#include <boost/preprocessor/repetition/repeat_from_to.hpp>
+#include <boost/preprocessor/repetition/repeat.hpp>
+#include <boost/preprocessor/tuple/elem.hpp>
+#include <boost/preprocessor/punctuation/comma.hpp>
 
 // Athena.Ciphers
 #include "detail/config.hpp"
@@ -27,8 +29,20 @@
 namespace athena {
 namespace ciphers {
 
+namespace detail {
+
+template <typename Alphabet, std::size_t X, std::size_t Y>
+struct unused;
+
+} // namespace detail
+
+template <
+    typename Alphabet,
+    template <typename, std::size_t, std::size_t> class mapper = detail::unused
+> struct table_array;
+
 template <typename Sequence>
-struct table_array
+struct table_array<Sequence, detail::unused>
 {
   static const char data[alphabet_number + 1];
 };
@@ -36,11 +50,46 @@ struct table_array
 template <typename Sequence>
 const char table_array<Sequence>::data[alphabet_number + 1] =
 {
-  BOOST_PP_REPEAT_FROM_TO(
-      0,
+  BOOST_PP_REPEAT(
       ATHENA_CIPHERS_ALPHABET_NUMBER,
       ATHENA_CIPHERS_DETAIL_RANDOM_ACCESS_PP_REPEAT_MACRO,
       Sequence)
+  '\0'
+};
+
+template <
+    typename Alphabet,
+    template <typename, std::size_t, std::size_t> class mapper
+> struct table_array
+{
+  static const char data[alphabet_number * alphabet_number + 1];
+};
+
+#define ATHENA_CIPHERS_TABLE_ARRAY_GET_SEQUENCE_PP_PEPEAT_MACRO(z, x, alp) \
+  BOOST_PP_REPEAT( \
+      ATHENA_CIPHERS_ALPHABET_NUMBER, \
+      ATHENA_CIPHERS_TABLE_ARRAY_GET_VALUE_PP_PEPEAT_MACRO, \
+      (alp, x)) \
+  /**/
+
+#define ATHENA_CIPHERS_TABLE_ARRAY_GET_VALUE_PP_PEPEAT_MACRO(z, y, tuple2) \
+  mapper< \
+      BOOST_PP_TUPLE_ELEM(2, 0, tuple2) BOOST_PP_COMMA() \
+      BOOST_PP_TUPLE_ELEM(2, 1, tuple2) BOOST_PP_COMMA() \
+      y>::value BOOST_PP_COMMA() \
+  /**/
+
+template <
+    typename Alphabet,
+    template <typename, std::size_t, std::size_t> class mapper
+>
+const char table_array<Alphabet, mapper>
+    ::data[alphabet_number * alphabet_number + 1] =
+{
+  BOOST_PP_REPEAT(
+      ATHENA_CIPHERS_ALPHABET_NUMBER,
+      ATHENA_CIPHERS_TABLE_ARRAY_GET_SEQUENCE_PP_PEPEAT_MACRO,
+      Alphabet)
   '\0'
 };
 
